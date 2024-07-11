6bd4b2bdd0235765cea94c638b8509a735361b30
bielwdev <bielwdev@gmail.com>
2024-07-11 15:38:43 -0300

    some texts added

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!
+          <div>
+
+          </div>
+        </p>
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>
+          com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade</a>.</p>
+      </div>
+    </div>
+
   )
 }
 
