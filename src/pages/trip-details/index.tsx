91c0413377a828b1833e78b8b98277804442feb4
bielwdev <bielwdev@gmail.com>
2024-08-17 15:30:06 -0300

    fix(trip-details): link to reservation changed from span to a a

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -72,8 +72,14 @@ export function TripDetailsPage(){
               <div className="flex items-center justify-between gap-4">
                 <div className="space-y-1.5">
                   <span className="block font-medium text-zinc-100">Reserva do Airbnb</span>
-                  <span className="block text-sm text-zinc-400 truncate"> https://www.airbnb.com.br/rooms/129378912389123978741278943</span>
-
+                  <a href="#" className="block text-sm text-zinc-400 truncate hover:text-zinc-200"> https://www.airbnb.com.br/rooms/129378912389123978741278943</a>
+                </div>
+                <Link2 className="text-zinc-400 size-5 shrink-0"/>
+              </div>
+              <div className="flex items-center justify-between gap-4">
+                <div className="space-y-1.5">
+                  <span className="block font-medium text-zinc-100">Reserva do Airbnb</span>
+                  <a href="#" className="block text-sm text-zinc-400 truncate hover:text-zinc-200"> https://www.airbnb.com.br/rooms/129378912389123978741278943</a>
                 </div>
                 <Link2 className="text-zinc-400 size-5 shrink-0"/>
               </div>
