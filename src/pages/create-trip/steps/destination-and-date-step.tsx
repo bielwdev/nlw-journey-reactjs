31ec9ab168d72d39f2da492e5ae58acfaeedcbaf
bielwdev <bielwdev@gmail.com>
2024-08-19 10:48:26 -0300

    feat(day-picker): add react daypicker component and replace input with modal

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,7 +1,10 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
-interface DestinationAndDateStepProps{
+interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean
   closeGuestInput: () => void
   openGuestsInput: () => void
@@ -12,32 +15,62 @@ export function DestinationAndDateStep({
   openGuestsInput,
   isGuestsInputOpen,
 }: DestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+
+  function openDatePicker() {
+    setIsDatePickerOpen(true)
+  }
+
+  function closeDatePicker() {
+    setIsDatePickerOpen(false)
+  }
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3 flex-1 ">
-            <div className='flex items-center gap-2 flex-1'>
-              <MapPin className='size-5 text-zinc-400' />
-              <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      <div className='flex items-center gap-2 flex-1'>
+        <MapPin className='size-5 text-zinc-400' />
+        <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      </div>
+
+      <button onClick={openDatePicker} disabled={isGuestsInputOpen} className='flex items-center gap-2 text-left'>
+        <Calendar className='size-5 text-zinc-400' />
+        <span className="text-lg text-zinc-400 w-40">
+          Quando?
+        </span>
+      </button>
+
+      {isDatePickerOpen && (
+        <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='w-[320
+          px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className='flex items-center justify-between'>
+                <h2 className='text-lg font-semibold'>Selecione a data</h2>
+                <button onClick={closeDatePicker}>
+                  <X className='size-5 text-zinc-400' />
+                </button>
+              </div>
             </div>
-
-            <div className='flex items-center gap-2'>
-              <Calendar className='size-5 text-zinc-400' />
-              <input disabled={isGuestsInputOpen} type="date" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
-            </div>
-
-            <div className='w-px h-6 bg-zinc-800'></div>
-
-            {isGuestsInputOpen ? (
-              <Button onClick={closeGuestInput} variant="secondary" size="default">
-                Alterar local/data
-                <Settings2 className='size-5' />
-              </Button>
-            ) : (
-                <Button onClick={openGuestsInput} variant="primary">
-                Continuar
-                <ArrowRight className='size-5' />
-                </Button>
-            )}
-
+            <DayPicker mode="range" />
           </div>
+        </div>
+      )}
+
+
+      <div className='w-px h-6 bg-zinc-800'></div>
+
+      {isGuestsInputOpen ? (
+        <Button onClick={closeGuestInput} variant="secondary" size="default">
+          Alterar local/data
+          <Settings2 className='size-5' />
+        </Button>
+      ) : (
+        <Button onClick={openGuestsInput} variant="primary">
+          Continuar
+          <ArrowRight className='size-5' />
+        </Button>
+      )}
+
+    </div>
   )
 }
